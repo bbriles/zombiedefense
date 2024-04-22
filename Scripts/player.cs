12f7511ccf729a26e4cc1eee85a6be3a12128920
Brad Briles <brilesb@gmail.com>
2024-04-21 21:30:11 -0700

    Added basic player controller.

---
+++ Scripts/player.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+	[Export]
+	public int Speed = 300;
+
+	public void GetInput()
+	{
+		Vector2 inputDirection = Input.GetVector("p1_left", "p1_right", "p1_up", "p1_down");
+        Velocity = inputDirection * Speed;
+
+		if (Velocity.X != 0 || Velocity.Y != 0)
+		{
+			Rotation = Velocity.Angle();
+		}
+	}
+
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
+}
